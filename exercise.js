4635254de831e786457562c86f56cbabcf473a96
cng008 <92824407cng008@users.noreply.github.com>
2021-12-27 19:41:41 -0800

    prompts answered

--- exercise.js
+++ exercise.js
@@ -5,24 +5,36 @@
 function createInstructor(firstName, lastName) {
 	return {
 		firstName: firstName,
-		lastName: lastName,
+		lastName: lastName
 	};
 }
 
 // Same keys and values ES2015
 /* Write an ES2015 Version */
+function createInstructor(firstName, lastName) {
+	return {
+		firstName,
+		lastName
+	};
+}
 
 // Computed Property Names
 var favoriteNumber = 42;
 
 var instructor = {
-	firstName: 'Colt',
+	firstName: 'Colt'
 };
 
 instructor[favoriteNumber] = 'That is my favorite!';
 
 // Computed Property Names ES2015
 /* Write an ES2015 Version */
+let favoriteNumber = 42;
+
+const instructor = {
+	[firstName]: 'Colt',
+	[favoriteNumber]: 'That is my favorite!'
+};
 
 // Object Methods
 var instructor = {
@@ -32,11 +44,20 @@ var instructor = {
 	},
 	sayBye: function () {
 		return this.firstName + ' says bye!';
-	},
+	}
 };
 
 // Object Methods ES2015
 /* Write an ES2015 Version */
+const instructor = {
+	firstName: 'Colt',
+	sayHi() {
+		return 'Hi!';
+	},
+	sayBye() {
+		return this.firstName + ' says bye!';
+	}
+};
 
 // createAnimal function
 /*Write a function which generates an animal object. The function should accepts 3 arguments:
@@ -44,7 +65,7 @@ var instructor = {
 species: the species of animal (‘cat’, ‘dog’)
 verb: a string used to name a function (‘bark’, ‘bleet’)
 noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
-Use one or more of the object enhancements we’ve covered.
+Use one or more of the object enhancements we’ve covered.*/
 
 const d = createAnimal('dog', 'bark', 'Woooof!');
 // {species: "dog", bark: ƒ}
@@ -52,4 +73,13 @@ d.bark(); //"Woooof!"
 
 const s = createAnimal('sheep', 'bleet', 'BAAAAaaaa');
 // {species: "sheep", bleet: ƒ}
-s.bleet(); //"BAAAAaaaa"*/
+s.bleet(); //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise) {
+	return {
+		species,
+		[verb]() {
+			return noise; //had in []
+		}
+	};
+}
